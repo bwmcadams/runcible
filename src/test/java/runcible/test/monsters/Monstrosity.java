a4fe97f2dc8d8af5a889741950350e13b080b50d
Max Afonov <max@bumnetworks.com>
2010-06-29 16:01:03 -0400

    filling out the tests

--- src/test/java/runcible/test/monsters/Monstrosity.java
+++ src/test/java/runcible/test/monsters/Monstrosity.java
@@ -1,7 +1,7 @@
 package runcible.test.monsters;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 
 /**
    A typical imperative, mutable Java class that so many (otherwise
@@ -10,7 +10,7 @@ import java.util.List;
 public class Monstrosity {
     private String field1;
     private int field2;
-    private List<Long> field3;
+    private Collection<Long> field3;
 
     public Monstrosity() {
         field1 = "some default value";
@@ -24,8 +24,8 @@ public class Monstrosity {
     public int getField2() { return field2; }
     public void setField2(int v) { field2 = v; }
 
-    public List<Long> getField3() { return field3; }
-    public void setField3(List<Long> v) { field3 = v; }
+    public Collection<Long> getField3() { return field3; }
+    public void setField3(Collection<Long> v) { field3 = v; }
 
     public String frobnicate() {
         return "ohai, " + field1 + " (" + field2
--- src/test/scala/specs.scala
+++ src/test/scala/specs.scala
@@ -1,20 +1,52 @@
 package runcible.test.specs
 
 import org.specs._
+import org.specs.specification.PendingUntilFixed
+
 import runcible.test.monsters.{Monstrosity, Syndicate}
 import runcible.annotation.{BeanMethods, Scalify}
 
+import scala.collection.JavaConversions._
+
 @Scalify(classOf[Monstrosity])
 @BeanMethods
-class Fluffy
+class ScalaMonstrosity extends Monstrosity {
+  override def getField1 = "wrong"
+  override def frobnicate = "even worse"
+}
 
-object RuncibleSpecTest extends Specification {
-  detailedDiffs()
+object RuncibleSpecTest extends Specification with PendingUntilFixed {
+  detailedDiffs
+  shareVariables
 
-  "a monstrosity" should {
-    "allow itself to be configured" in {}
-    "frobnicate configured values" in {}
+  val ONE   = "ouch, scary"
+  val TWO   = 123456
+  val THREE = 1L :: 2L :: 3L :: Nil
+  val FROB  = "ohai, " + ONE + " (" + TWO + "), but also [" + THREE.mkString(", ") + "]!"
+
+  def what_a_monstrosity_does(monster: Monstrosity) = {
+    "allow itself to be configured" in {
+      monster.setField1(ONE)
+      monster.setField2(TWO)
+      monster.setField3(THREE.map(Long.box))
+
+      monster.getField1 must_== ONE
+      monster.getField2 must_== TWO
+      monster.getField3.toList must containAll(THREE)
+    }
+
+    "frobnicate configured values" in {
+      monster.frobnicate must_== FROB
+    }
   }
+
+  "a Java monstrosity" should {
+    "be itself" in { what_a_monstrosity_does(new Monstrosity) }
+  }
+
+  "a more pleasant Scala monstrosity" should {
+    "behave like its Java counterpart" in { what_a_monstrosity_does(new ScalaMonstrosity) }
+  } pendingUntilFixed
 
   "a syndicate" should {
     "rake in a number of monstrosities" in {}
